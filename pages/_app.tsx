4d0c49a61eb8ef109ffb86d6bd3fdbf868f899b7
Qikta <qikta.technolo7545@gmail.com>
2022-01-22 22:28:04 +0900

    create pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import ModalContent from '../components/ModalContent'
+import useModal from '../hooks/useModal'
+import { createContext, useCallback, useState } from 'react'
+
+export const ModalContext = createContext<[]>({})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // const { toggle, open, close } = useModal()
+  const [toggle, setToggle] = useState(false)
+
+  const open = () => {
+    console.log('open');
+    
+    setToggle(true)}
+
+  return (
+    <>
+       <Header onClick={open} />
+        <Component {...pageProps} />
+        { toggle && <ModalContent /> }
+        <Footer />
+    </>
+  ) 
 }
 
 export default MyApp
