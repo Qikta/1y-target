68af6f5eff5de4c625672eb1eb0e376f4c6a9682
Qikta <qikta.technolo7545@gmail.com>
2022-01-25 22:37:10 +0900

    add getProfile

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,9 +1,22 @@
 import { SetStateAction, useEffect, useState } from 'react';
+import { definitions } from '../types/entities/supabase';
 import { supabase } from '../utils/supabaseClient'
+
+export interface IProfile {
+  id: string
+  username?: string
+  avatar_url?: string
+  self_description?: string
+  twitter_url?: string
+  instagram_url?: string
+  website?: string
+}
 
 export default function useUser() {
   const [session, setSession] = useState()
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [profile, setProfile] = useState<IProfile>();
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
@@ -36,6 +49,58 @@ export default function useUser() {
   //   setupUser();
   // }, [session]);
 
+  useEffect(() => {
+    const setUpProfile = async () => {
+      const user = supabase.auth.user()
+      if (user) {
+        try {
+          setLoading(true);
+          let { data, error, status } = await supabase
+            .from<definitions['profiles']>('profiles')
+            .select('*')
+            .eq('id', user?.id)
+            .single()
+  
+          if (error && status !== 406) {
+            throw error
+          }
+  
+          if (data) {
+            const profile: IProfile = {
+              id: user?.id || '',
+              username: data.username,
+              avatar_url: data.avatar_url,
+              self_description: '',
+              twitter_url: data.twitter_url,
+              instagram_url: data.instaegram_url,
+              website: data.website
+            }
+            setProfile(profile)
+          }
+        } catch(err) {
+          alert(err)
+        } finally {
+          setLoading(false)
+        }
+      }
+      setUpProfile()
+    }
+  }, [])
+
+  const insertProfile =async (request: IProfile) => {
+    const user = supabase.auth.user()
+
+    if (user !== null) {
+      setLoading(true);
+
+      const { error } = await supabase.from('profiles').insert([request])
+
+      if (error) { throw error}
+
+        location.reload()
+    }
+  }
+
   function signInWithGoogle() {
     supabase.auth.signIn({ provider: "google" });
   }
@@ -55,5 +120,7 @@ export default function useUser() {
     signInWithGoogle,
     signInWithGithub,
     signOut,
+    profile,
+    insertProfile
   };
 }
