0cbed2a82c1cbb9581b41eca0f1c2665b1163a4b
Qikta <qikta.technolo7545@gmail.com>
2022-01-28 19:14:50 +0900

    fix Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import path from 'path';
 
 export default function Footer () {
   return (
-    <footer className={styles.footer}>
-        <a
+    <footer className="text-center lg:text-left bg-gray-100 text-gray-600">
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -13,7 +14,24 @@ export default function Footer () {
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
+        </a> */}
+        <div>
+          <ul>
+            <li>
+              <a href="">Privacy</a>
+            </li>
+            <li>
+              <a href="">terms</a>
+            </li>
+          </ul>
+        </div>
+        <div className="text-gray-700 text-center p-8">
+          © 2022 Copyright:
+          <span className={styles.logo}>
+          </span>
+          <a className="text-gray-800" href="https://localhost:3000/">One Year Target</a>
+        </div>
+
       </footer>
   )
 }
