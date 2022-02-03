5180648789e559c0dc5dfa5c567a16456cbeaaf6
Qikta <qikta.technolo7545@gmail.com>
2022-02-04 01:05:45 +0900

    add userpage

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Target from '../../components/Target'
+import { Context } from '../../components/TargetList'
+import { ITarget } from '../../hooks/useTarget'
 import { definitions } from '../../types/entities/supabase'
 import { supabase } from '../../utils/supabaseClient'
 interface PathParams {
@@ -60,9 +63,49 @@ export const getStaticProps = async ({ params }) => {
 
 // @ts-ignore
 const Main = ({post}) => {
+  const seriarisePost = JSON.parse(post)
+  const targetList: Array<ITarget> = []
+
+  if(seriarisePost) {
+    for (const target of seriarisePost) {
+      const optionalCreateTiem = target.created_at !== undefined ? new Date(target.created_at) : undefined
+
+      targetList.push({
+        id: String(target.id),
+        name: target?.name || '',
+        user_name: target.username || '',
+        description: target.description || '',
+        value: target.value || 0,
+        is_complete: target.is_complete || false,
+        favorite_count: target.favorite_count || 0,
+        avater_url: target.avatar_url || '',
+        created_date: optionalCreateTiem?.toLocaleDateString() || '',
+        ogp_url: target.ogp_url
+      })
+    }
+  }
+  const userInfo: ITarget = targetList[0]
+
   return (
-    <div>
-      <h2>dadffd</h2>
+    <div className='container my-8 mx-auto px-4 md:px-12'>
+      <div className='flex justify-center mx-auto'>
+        <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={userInfo.avater_url} />
+        <div className="flex flex-col items-center text-center justify-center">
+          <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ userInfo.user_name }</h2>
+          <div className="w-12 h-1 bg-amber-400 rounded mt-2 mb-4"></div>
+          <p className="text-base">user description</p>
+          <p>{targetList.length}Target</p>
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-1 lg:-mx-4">
+        {
+          targetList.map((target, index) => (
+            <Context.Provider value={target} key={index} >
+              <Target />
+            </Context.Provider>
+          ))
+        }
+      </div>
     </div>
   )
 }
