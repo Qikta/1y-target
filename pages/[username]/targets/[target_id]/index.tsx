45ef97812f4e1bd8092b8a6fb2ba407a4d800391
Qikta <qikta.technolo7545@gmail.com>
2022-02-03 17:23:30 +0900

    add twitter share button

--- pages/[username]/targets/[target_id]/index.tsx
+++ pages/[username]/targets/[target_id]/index.tsx
@@ -1,10 +1,12 @@
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { ITarget } from "../../../../hooks/useTarget";
 import { definitions } from "../../../../types/entities/supabase";
 import { supabase } from "../../../../utils/supabaseClient";
 import dynamic from "next/dynamic";
 import { HeatMapValue } from "@uiw/react-heat-map";
+import { TwitterShareButton, FacebookShareButton, LineShareButton, HatenaShareButton, TwitterIcon, FacebookIcon, LineIcon, HatenaIcon } from "react-share";
+import Seo from "../../../../components/SeoHeader"
 
 // @ts-ignore
 const HeatMap = dynamic(() => import("@uiw/react-heat-map").then((mod) => mod.default),{ ssr: false });
@@ -57,7 +59,7 @@ export const getStaticProps = async ({ params }) => {
 // @ts-ignore
 const Main = ({ post}) => {
   const seriarisePost = JSON.parse(post)
-
+  const router = useRouter()
   const optionalCreateTiem = seriarisePost.created_at !== undefined ? new Date(seriarisePost.created_at) : undefined
 
   const target: ITarget = {
@@ -82,52 +84,69 @@ const Main = ({ post}) => {
     { date: '2022/05/04', count:11, content: '' },
     { date: '2022/05/08', count:32, content: '' }
   ];
-  
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? '';
   
   return (
-    <div className="container px-5 py-24 mx-auto flex flex-col">
-      <div className="lg:w-4/6 mx-auto">
-        <div className="overflow-hidden">
-          { target.ogp_url &&
-            <img alt="content" className="object-cover object-center h-full w-full" src={target.ogp_url} />
-          }
-        </div>
-        <div className="flex flex-col sm:flex-row mt-10">
-          <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-            <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={target.avater_url} />
-            <div className="flex flex-col items-center text-center justify-center">
-              <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ target.user_name }</h2>
-              <div className="w-12 h-1 bg-amber-400 rounded mt-2 mb-4"></div>
-              <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+    <>
+      <Seo 
+        pageTitle={ target.name }
+        pageDescription={ target.description }
+        pagePath={`${baseUrl}${router.asPath}`}
+        pageImg={ target.ogp_url}
+        pageImgWidth={1280}
+        pageImgHeight={960}
+      />
+      <div className="container px-5 py-24 mx-auto flex flex-col">
+        <div className="lg:w-4/6 mx-auto">
+          <div className="overflow-hidden">
+            { target.ogp_url &&
+              <img alt="content" className="object-cover object-center h-full w-full" src={target.ogp_url} />
+            }
+          </div>
+          <div className="flex flex-col sm:flex-row mt-10">
+            <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+              <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={target.avater_url} />
+              <div className="flex flex-col items-center text-center justify-center">
+                <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ target.user_name }</h2>
+                <div className="w-12 h-1 bg-amber-400 rounded mt-2 mb-4"></div>
+                <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+              </div>
+            </div>
+            <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+              <p className="leading-relaxed text-lg mb-4">
+                { target.description }
+              </p>
             </div>
           </div>
-          <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-            <p className="leading-relaxed text-lg mb-4">
-              { target.description }
-            </p>
+        </div>
+        <div className="lg:w-4/6 mx-auto">
+          <div className="flex flex-col sm:flex-row mx-auto mt-10 text-center">
+            {/* @ts-ignore */}
+            <HeatMap value={ heatmapValue }
+              width={725}
+              style={{ color: '#ad001d' }}
+              panelColors={{
+                0: '#f4decd',
+                2: '#e4b293',
+                4: '#d48462',
+                10: '#c2533a',
+                20: '#ad001d',
+                30: '#000',
+              }}
+              weekLabels={[,'Mon',,'Wed',,'Fri',]}
+              startDate={new Date('2022/01/01')}
+            />
           </div>
         </div>
-      </div>
-      <div className="lg:w-4/6 mx-auto">
-        <div className="flex flex-col sm:flex-row mx-auto mt-10 text-center">
-          {/* @ts-ignore */}
-          <HeatMap value={ heatmapValue }
-            width={725}
-            style={{ color: '#ad001d' }}
-            panelColors={{
-              0: '#f4decd',
-              2: '#e4b293',
-              4: '#d48462',
-              10: '#c2533a',
-              20: '#ad001d',
-              30: '#000',
-            }}
-            weekLabels={[,'Mon',,'Wed',,'Fri',]}
-            startDate={new Date('2022/01/01')}
-          />
+        <div>
+        <TwitterShareButton url={`${baseUrl}${router.asPath}`} title={ target.name }>
+          <TwitterIcon size={30} round={true} />
+        </TwitterShareButton>
         </div>
       </div>
-    </div>
+    </>
+    
   )
 }
 
