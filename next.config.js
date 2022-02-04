5a9f86a897c3d63fa740d652b159b3d411e97eb3
Qikta <qikta.technolo7545@gmail.com>
2022-02-04 23:28:04 +0900

    add config

--- next.config.js
+++ next.config.js
@@ -10,3 +10,14 @@ const removeImports = require("next-remove-imports")({
 });
 
 module.exports = removeImports()
+
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
