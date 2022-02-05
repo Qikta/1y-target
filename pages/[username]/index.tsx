9648f552eaddd55aab63089cf821da1125f55047
Qikta <qikta.technolo7545@gmail.com>
2022-02-05 17:35:27 +0900

    fix userInfo

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -66,7 +66,6 @@ export const getServerSideProps = async ({ params }) => {
 const Main = ({post}) => {
   const seriarisePost = JSON.parse(post)
   const targetList: Array<ITarget> = []
-  const { profile } = useUser()
 
   if(seriarisePost) {
     for (const target of seriarisePost) {
@@ -91,11 +90,11 @@ const Main = ({post}) => {
   return (
     <div className='container my-8 mx-auto px-4 md:px-12'>
       <div className='flex justify-center mx-auto'>
-        { profile?.avatar_url &&
-          <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={profile.avatar_url} />
+        { userInfo.avater_url &&
+          <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={userInfo.avater_url} />
         }
         <div className="flex flex-col items-center text-center justify-center">
-          <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ profile?.username }</h2>
+          <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ userInfo.user_name }</h2>
           <div className="w-12 h-1 bg-amber-400 rounded mt-2 mb-4"></div>
           <p className="text-base">user description</p>
           <p>{targetList.length}Target</p>
