8a9544cf11751972dc152e4a01130da1d930127b
Qikta <qikta.technolo7545@gmail.com>
2022-02-06 14:42:15 +0900

    add hover style

--- components/Target.tsx
+++ components/Target.tsx
@@ -10,13 +10,13 @@ export default function Target () {
     const router = useRouter()
 
     const targetStyle = !targetContext.is_complete ? 
-      'overflow-hidden rounded-lg shadow-lg' : 'overflow-hidden rounded-lg shadow-lg outline outline-green-400'
+      'overflow-hidden rounded-lg shadow-lg hover:outline hover:outline-amber-400' : 'overflow-hidden rounded-lg shadow-lg hover:outline hover:outline-green-400'
 
   return (
     <div className="my-1 px-1 w-full sm:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       <Link href={{ pathname: '/[username]/targets/[target_id]',
             query: { username: targetContext.user_name, target_id: targetContext.id }}} passHref>
-        <article className={targetStyle}>
+        <article className='overflow-hidden rounded-lg shadow-lg hover:outline hover:outline-amber-400'>
           { targetContext.ogp_url &&
             <img className="block h-auto w-full" src={targetContext.ogp_url} alt="ogpImage" />
           }
@@ -37,8 +37,25 @@ export default function Target () {
           <footer className="flex items-center justify-between leading-none p-2 md:p-4">
             <div className="flex items-center no-underline hover:underline text-black">
               <Link href={{pathname: '/[username]', query: { username: targetContext.user_name }}} passHref>
-                { targetContext.avater_url &&
+                { targetContext.avater_url ?
                   <img className="w-10 h-10 rounded-full mr-4" src={targetContext.avater_url} alt="Avatar of Jonathan Reinink" />
+                  :
+                  <button className="rounded-full bg-gray-200">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-gray-500 m-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                  </button>
                 }
               </Link>
               <div className="text-sm">
--- components/TargetList.tsx
+++ components/TargetList.tsx
@@ -20,9 +20,15 @@ export default function TargetList() {
   const { targetList, loading } = useTarget()
   const [stateProp, setStateProp] = useState<ITarget>()
 
-if (loading) return <div>loading...</div>
-if (!targetList.length) return <div>missing data...</div>
-  return (
+if (loading) return (
+  <div className="flex justify-center py-16 ">
+    <div className="animate-spin h-10 w-10 border-4 border-amber-500 rounded-full border-t-transparent"></div>
+  </div>
+)
+
+if (!targetList.length) return <div className="container my-8 mx-auto px-4 md:px-12">missing data...</div>
+
+return (
     <div className="container my-8 mx-auto px-4 md:px-12">
       <div className="flex flex-wrap -mx-1 lg:-mx-4">
          <div className="w-full px-4">
