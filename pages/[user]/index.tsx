21abc7f595aa4284b0c16abf8d4fb9ae4db11116
Qikta <qikta.technolo7545@gmail.com>
2022-02-11 03:19:01 +0900

    fix userInfo

--- pages/[user]/index.tsx
+++ pages/[user]/index.tsx
@@ -21,16 +21,16 @@ const Main = () => {
   return (
     <div className='container my-8 mx-auto px-4 md:px-12'>
       <div className='flex justify-center mx-auto'>
-        { userInfo.avater_url &&
-          <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={userInfo.avater_url} />
+        { userInfo?.avater_url &&
+          <img alt="content" className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400" src={userInfo?.avater_url} />
         }
         <div className="flex flex-col items-center text-center justify-center">
-          <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ userInfo.user_name }</h2>
+          <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">{ userInfo?.user_name }</h2>
           <div className="w-12 h-1 bg-amber-400 rounded mt-2 mb-4"></div>
           <p className="text-base">user description</p>
           <p>{userTargetList.length}Target</p>
         </div>
-        { profile?.user_name == userInfo.user_name && 
+        { profile?.user_name == userInfo?.user_name && 
         <div className="flex flex-col items-center text-center justify-end ">
           <button onClick={() => router.push('/settings/profile')}>edit</button>
         </div>
