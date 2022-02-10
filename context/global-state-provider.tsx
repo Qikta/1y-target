5583e6aadb25dd60068d5f84bf8fd66c0a5f96ce
Qikta <qikta.technolo7545@gmail.com>
2022-02-11 03:08:58 +0900

    add profile for global context

--- context/global-state-provider.tsx
+++ context/global-state-provider.tsx
@@ -1,20 +1,34 @@
-import React, { Children, useState, createContext, } from "react";
+import { Session } from "@supabase/supabase-js";
+import React, { Children, useState, createContext, useEffect, } from "react";
 import useTarget, { ITarget } from "../hooks/useTarget";
+import useUser, { IProfile } from "../hooks/useUser";
 
 export interface GlobalState {
   targetList: Array<ITarget>
+  profile?: IProfile
+//   session: Session | null
 }
 
 export const GlobalContext = createContext<GlobalState>({
   targetList: [],
-
+  profile: {
+    id: '',
+    user_name: '',
+    avatar_url: '',
+    self_description: '',
+    twitter_url: '',
+    instagram_url: '',
+    website: ''
+  },
+//   session: null
 })
 
 const GlobalStateProvider = ({children}: {children: React.ReactNode}) => {
 //   const [state, setState] = useState<GlobalState>()
   const { targetList } = useTarget()
+  const { profile } = useUser()
 
-  const global = {targetList}
+  const global = {targetList, profile }
 
   return (
     <GlobalContext.Provider value={global}>{children}</GlobalContext.Provider>
