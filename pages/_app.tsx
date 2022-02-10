a42b4b2e7b49cbc3896d20c45e28efd20da22a34
Qikta <qikta.technolo7545@gmail.com>
2022-02-11 03:09:57 +0900

    add seoHeader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,22 @@ import { createContext, useCallback, useState } from 'react'
 import useUser from '../hooks/useUser'
 import '@uiw/react-heat-map/dist.css';
 import GlobalStateProvider from '../context/global-state-provider'
+import Seo from '../components/SeoHeader'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { toggle, open, close } = useModal()
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? '';
 
   return (
     <>
       <GlobalStateProvider>
+        <Seo
+          pageTitle='One Year Target'
+          pageDescription='One Year Target'
+          pagePath={`${baseUrl}`}
+          pageImgWidth={1280}
+          pageImgHeight={960}
+        />
         <Header onClick={open} />
         <Component {...pageProps} onClick={open} />
         { toggle && <ModalContent onClick={close} /> }
