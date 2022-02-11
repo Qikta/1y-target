3de33bfd86670bcc554f75072befd075067aa488
Qikta <qikta.technolo7545@gmail.com>
2022-02-11 14:48:58 +0900

    fix footer design and add page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,35 +3,45 @@ import Image from 'next/image'
 import path from 'path';
 
 export default function Footer () {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
   return (
-    <footer className="text-center lg:text-left bg-gray-100 text-gray-600">
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a> */}
-        <div>
-          <ul>
-            <li>
-              <a href="">Privacy</a>
-            </li>
-            <li>
-              <a href="">terms</a>
-            </li>
-          </ul>
+    <footer className="footer bg-gray-100 relative pt-1 border-b-2">
+      <div className="container mx-auto px-6">
+        <div className="sm:flex sm:mt-8">
+          <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700 uppercase mb-2">About</span>
+              <span className="my-2"><a href="about" className="text-gray-700 text-md hover:text-gray-500">OYTについて</a></span>
+              {/* <span className="my-2"><a href="#" className="text-blue-700  text-md hover:text-blue-500">link 1</a></span>
+              <span className="my-2"><a href="#" className="text-blue-700  text-md hover:text-blue-500">link 1</a></span> */}
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Legal</span>
+              <span className="my-2"><a href="terms" className="text-gray-700 text-md hover:text-gray-500">利用規約</a></span>
+              <span className="my-2"><a href="privacy" className="text-gray-700 text-md hover:text-gray-500">Privacy Policy</a></span>
+              {/* <span className="my-2"><a href="#" className="text-blue-700  text-md hover:text-blue-500">link 1</a></span>
+              <span className="my-2"><a href="#" className="text-blue-700 text-md hover:text-blue-500">link 1</a></span> */}
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Contact</span>
+              <span className="my-2"><a href="https://twitter.com/qikta" className="text-gray-700 text-md hover:text-gray-500">Twitter</a></span>
+              {/* <span className="my-2"><a href="#" className="text-blue-700  text-md hover:text-blue-500">link 1</a></span>
+              <span className="my-2"><a href="#" className="text-blue-700  text-md hover:text-blue-500">link 1</a></span> */}
+            </div>
+          </div>
         </div>
-        <div className="text-gray-700 text-center p-8">
-          © 2022 Copyright:
-          <span className={styles.logo}>
-          </span>
-          <a className="text-gray-800" href="https://localhost:3000/">One Year Target</a>
+      </div>
+      <div className="container mx-auto px-6">
+        <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+          <div className="sm:w-2/3 text-center py-6">
+              <p className="text-sm text-gray-700 font-bold mb-2">
+              © 2022 Copyright:
+              <span className={styles.logo} />
+              <a className="text-gray-800" href={baseUrl}>One Year Target</a>
+              </p>
+          </div>
         </div>
-
-      </footer>
+      </div>
+    </footer>
   )
 }
---
+++ pages/terms.tsx
@@ -0,0 +1,11 @@
+
+
+const Terms =() => {
+  return (
+    <div>
+      Terms page...
+    </div>
+  )
+}
+
+export default Terms
