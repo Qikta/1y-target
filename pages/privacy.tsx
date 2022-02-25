8c43ef89efc75ae326f04df6e0bb92736996cbad
Qikta <qikta.technolo7545@gmail.com>
2022-02-25 21:31:28 +0900

    fix privacy policy page

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,8 +1,24 @@
+import path from 'path';
+import fs from 'fs';
+import ReactMarkdown from 'react-markdown';
 
-const Privacy = () => {
+export const getStaticProps = async () => {
+  const markdown = fs.readFileSync(
+    path.join(process.cwd(), '/public/md/privacy.md'),
+    'utf8',
+  );
+  return {
+    props: { markdown },
+  };
+};
+const Privacy = ({ markdown }: { markdown: string }) => {
   return (
-    <div>
-      Privacy page...
+    <div className="container mx-auto rounded-full">
+      <h1 className="text-center text-3xl py-5">Privacy Policy</h1>
+      <div className="border-t py-10 border-gray-200 markdown">
+        {/* @ts-ignore */}
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
     </div>
   )
 }
