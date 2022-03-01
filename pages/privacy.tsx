a0402c0da7f5c50b07cb63c5b9ab09cc31906a84
Qikta <qikta.technolo7545@gmail.com>
2022-03-01 20:22:25 +0900

    add navigation bar

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -13,11 +13,25 @@ export const getStaticProps = async () => {
 };
 const Privacy = ({ markdown }: { markdown: string }) => {
   return (
-    <div className="container mx-auto ounded-md p-5 my-5 bg-gray-100">
-      <h1 className="text-center text-3xl py-5">Privacy Policy</h1>
-      <div className="border-t py-10 border-gray-200 markdown">
-        {/* @ts-ignore */}
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+    <div className="container mx-auto">
+      <div className="mb-8">
+        <div className="flex flex-wrap -mb-px" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+            <a className="mr-2" role="presentation" href={'/terms'}>
+                <button className="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Terms</button>
+            </a>
+            <a className="mr-2" role="presentation" href={'/privacy'}>
+                <button className="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300 active" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="true">
+                  Privacy
+                </button>
+            </a>
+        </div>
+      </div>
+      <div className='rounded-md p-5 my-5 bg-gray-100'>
+        <h1 className="text-center text-3xl py-5">Privacy Policy</h1>
+        <div className="border-t py-10 border-gray-200 markdown">
+          {/* @ts-ignore */}
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
       </div>
     </div>
   )
